d383220a87876ef3817209fbcae93b92944e050d
burakcan <bcbulbul1606@gmail.com>
2016-03-09 17:13:33 +0200

    Development Commit

--- citation_parser.py
+++ citation_parser.py
@@ -65,27 +65,49 @@ class ParserScholar(Author):
         get_href_list=self.get_quotes_href_link()
         print(get_href_list)
         get_quotes_list=[]
+        counter=0
         for read_href in get_href_list:
-            r=requests.get(read_href)
-            if r.url is None:
-                continue
-            else:
-                if r.status_code==200:
-                    soup=BeautifulSoup(r.content,'html.parser')
-                    for quotes in soup.find_all('div','gs_rs'):
-                     get_quotes_list.append(quotes.string)
+         r=requests.get(read_href)
+         print(r.url)
+        if r.status_code==200:
+                soup=BeautifulSoup(r.content,'html.parser')
+                for quotes in soup.find_all('div','gs_rs'):
+                 get_quotes_list.append(quotes.string)
+                counter+=1
 
-            get_quotes_list.append("\n ----------------------------------")
+        get_quotes_list.append("\n ----------------------------------")
 
         for i in range(get_quotes_list.__len__()):
             print(get_quotes_list.__getitem__(i))
+        self.get_quotes_length(counter)
         return get_quotes_list
 
 
-    def isUpgrade(self,control):
-        control=False
+    def get_quotes_length(self,length):
+        self.isUpgrade(length)
+        return length;
 
-        return control
+    def is_upgrade(self,old_length):
+        is_equal=False
+        get_href_list=self.get_quotes_href_link()
+        is_upgrade_list=[]
+        for read_href in get_href_list:
+            r=requests.get(read_href)
+            if r.url is not None and r.status_code==200:
+                soup=BeautifulSoup(r.content,'html.parser')
+                for quotes in soup.find_all('div','gs_rs'):
+                    is_upgrade_list.append(quotes.string)
+                    if len(is_upgrade_list) is not old_length:
+                        is_equal=False
+                    elif len(is_upgrade_list)>old_length:
+                        is_equal=True
+                        if is_equal is True:
+                            print("Updating...")
+                            return is_upgrade_list
+                        else:
+                            return self.get_quotes()
+
+        # eğer gelen length ile yeni length farklıysa,güncellenmesi gerekir. tekrar okuruz ve listeyi yenileriz.
 
 
 
